093026bdf646837edf8fd7a3d5e1d4c4461a4bf9
Hasan Sattar <hasanraza1798@gmail.com>
2024-05-08 19:54:15 +0100

    updated middleware to protect api routes and added ref to scroll last message into view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import axios from 'axios'
 
 export default function Home() {
 
+  const lastMessageRef = useRef<HTMLDivElement>(null)
   const [userInput, setUserInput] = useState('')
   const [messages, setMessages] = useState<IMessage[]>([])
   const [threadId, setThreadId] = useState('')
@@ -12,9 +13,9 @@ export default function Home() {
 
   const handleSendMessage = async () => {
     if (userInput.trim().length > 0 ) {
-      const response = await axios.post('/assistant', { threadId: '123' })
-      const threadId = response.data.threadId
-      localStorage.setItem('threadId', threadId)
+      const response = await axios.post('/assistant', { threadId })
+      const threadIdGenerated = response.data.threadId
+      localStorage.setItem('threadId', threadIdGenerated)
       const messagesToSave = JSON.stringify([...messages, { role: 'user', content: userInput }])
       localStorage.setItem('messages', messagesToSave)
       setMessages(prev => [...prev, { role: 'user', content: userInput }])
@@ -31,6 +32,10 @@ export default function Home() {
     setLoading(false)
   }, [])
 
+  useEffect(() => {
+    lastMessageRef.current && lastMessageRef.current.scrollIntoView({ behavior: 'smooth' })
+  }, [messages])
+
   return (
     <div className="relative">
     <nav className="fixed top-0 left-0 right-0 py-4 border-b-2 border-gray-400">
@@ -40,7 +45,7 @@ export default function Home() {
     { messages.length > 0 ? (
       <div className="mt-28">
       { messages.map(({ role, content }, index) => (
-        <div key={index} className={ role === 'user' ? "chat chat-end" : "chat chat-start"}>
+        <div key={index} ref={index === messages.length - 1 ? lastMessageRef : null} className={ role === 'user' ? "chat chat-end" : "chat chat-start"}>
           <p className={ role === 'user' ? "chat-bubble" : "chat-bubble chat-bubble-info"}>{content}</p>
         </div>
       )) }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
 const isProtectedRoute = createRouteMatcher([
-    '/'
+    '/',
+    '/assistant',
+    '/assistant-status'
 ])
 
 export default clerkMiddleware((auth, req) => {
