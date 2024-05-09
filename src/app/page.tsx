429a60bd8bbb816a2efe74e0d9cd9334e899d3f2
Hasan Sattar <hasanraza1798@gmail.com>
2024-05-09 15:56:59 +0100

    made voice interface more responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -166,7 +166,7 @@ export default function Home() {
           </div>
         ) }
 
-        { listening && <div ref={voiceMessageRef} className="flex flex-col divide-y items-center justify-center max-w-[50%] bg-white rounded-lg pt-4 text-gray-800 mx-auto">
+        { listening && <div ref={voiceMessageRef} className="flex flex-col divide-y items-center justify-center max-w-[95%] sm:max-w-[75%] md:max-w-[50%] bg-white rounded-lg pt-4 text-gray-800 mx-auto">
           <div className="flex flex-col justify-center items-center">
             <svg className="w-28 h-28" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
